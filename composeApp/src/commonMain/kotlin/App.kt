ce6dc41eb52a149dbb50bf93fd6af0350c5ee855
Martinez <amartin5@th-koeln.de>
2023-12-15 15:25:31 +0100

    Upload Expect Actual Prototype

---
+++ composeApp/src/commonMain/kotlin/App.kt
@@ -0,0 +1,35 @@
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.material.MaterialTheme
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.DragData
+import androidx.compose.ui.Modifier
+import org.jetbrains.compose.resources.ExperimentalResourceApi
+import androidx.compose.foundation.Image
+
+@Composable
+fun App() {
+    MaterialTheme {
+        var currentLocation by remember { mutableStateOf<Location?>(null) }
+
+        Column(Modifier.fillMaxWidth(), horizontalAlignment = Alignment.CenterHorizontally) {
+
+            locationButton("OMA") { currentLocation = Location.OMA }
+            locationButton("KEVIN") { currentLocation = Location.KEVIN }
+            locationButton("SCHULE") { currentLocation = Location.SCHULE }
+
+
+            currentLocation?.let { location ->
+                locationTest(location)
+            }
+        }
+    }
+}
+
+@Composable
+expect fun callImage(location : Location)
